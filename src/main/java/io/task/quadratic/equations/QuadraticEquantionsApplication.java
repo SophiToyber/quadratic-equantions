119d601735b8aa94555c347706b54793545a787f
SophiToyber <prosvitlyuk@ukr.net>
2021-03-12 17:07:12 +0200

    Initial commit

---
+++ src/main/java/io/task/quadratic/equations/QuadraticEquantionsApplication.java
@@ -0,0 +1,13 @@
+package io.task.quadratic.equations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadraticEquantionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuadraticEquantionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/task/quadratic/equations/QuadraticEquantionsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.task.quadratic.equations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuadraticEquantionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
